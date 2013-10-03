dd6be5b9f398956868fff92d9c1d901b05cc16b4
Nicolas Leroux <leroux.nicolas@gmail.com>
2013-10-03 21:03:04 +0200

    Update to 2.2. Add initial data for Tags. Have the example actually workling

--- app/models/Tag.java
+++ app/models/Tag.java
@@ -10,22 +10,17 @@ import java.util.List;
 @Entity
 public class Tag extends Model {
 
-  public static Finder<Long, Tag> find() {
-    return new Finder<>(Long.class, Tag.class);
-  }
-
   public static Tag findById(Long id) {
-    return find().byId(id);
+    return find.byId(id);
   }
 
   @Id
   public Long id;
   public String name;
 
-  @ManyToMany(mappedBy = "tags")
-  public List<Product> products;
-
-
+  public static Finder<Long, Tag> find =
+    new Finder<Long, Tag>(Long.class, Tag.class);
+  
   public Tag(){
     // Left empty
   }
@@ -33,7 +28,6 @@ public class Tag extends Model {
   public Tag(Long id, String name, Collection<Product> products) {
     this.id = id;
     this.name = name;
-    this.products = new LinkedList<>(products);
     for (Product product : products) {
       product.tags.add(this);
     }
